35cea3715941f2d7e059a7ad43e98432fb1e2160
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-06-17 00:57:32 +0530

    script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,6 @@
-// let js = 'amazing';
+let js = 'amazing';
 
-/*console.log(10 + 26 + 30 - 20);
+console.log(10 + 26 + 30 - 20);
 console.log("Shubham");
 let firstName = "Shubham";
 console.log(firstName);
@@ -11,36 +11,36 @@ console.log(firstName);
 let firstLan = 'C';
 let currentLan = 'java';
 console.log(firstLan);
-console.log(currentLan);*/
-// let continent = "Asia";
-// let countryName = "India";
-// let population = "150";
+console.log(currentLan);
+let continent = "Asia";
+let countryName = "India";
+let population = "150";
 
-// console.log(continent);
-// console.log(countryName);
-// console.log(population);
+console.log(continent);
+console.log(countryName);
+console.log(population);
 
-// let javascriptIsFun = true;
-// console.log(javascriptIsFun);
-// console.log(typeof true)
-// console.log(typeof javascriptIsFun);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true)
+console.log(typeof javascriptIsFun);
 
-// javascriptIsFun = "yes";
-// console.log(typeof javascriptIsFun);
-// let year;
-// console.log(year);
-// console.log(typeof year);
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
 
-// year = 1990;
-// console.log(typeof year);
-// console.log(typeof null);
+year = 1990;
+console.log(typeof year);
+console.log(typeof null);
 
-// let isIsland = true;
-// let language;
-// console.log(typeof isIsland);
-// console.log(typeof language);
-// console.log(typeof countryName);
-// console.log(typeof population);
+let isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof countryName);
+console.log(typeof population);
 
 const language = "hindi";
 var age = "18";
