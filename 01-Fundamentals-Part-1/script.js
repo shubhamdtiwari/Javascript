13e7a04dcda5cfd7340c2e04ccfb269f2e7606cf
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-06-20 23:34:18 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,91 @@
+let js = 'amazing';
+
+console.log(10 + 26 + 30 - 20);
+console.log("Shubham");
+let firstName = "Shubham";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let firstLan = 'C';
+let currentLan = 'java';
+console.log(firstLan);
+console.log(currentLan);
+let continent = "Asia";
+let countryName = "India";
+let population = "150";
+
+console.log(continent);
+console.log(countryName);
+console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true)
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1990;
+console.log(typeof year);
+console.log(typeof null);
+
+let isIsland = true;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof countryName);
+console.log(typeof population);
+
+const language = "hindi";
+var age = "18";
+age = "20"
+const dob = "2006";
+console.log(language);
+console.log(typeof language);
+console.log(typeof dob);
+console.log(age);
+
+// Math oprators
+const now = 2037;
+const ageShubham = now - 2006;
+const ageShub = now - 2007;
+console.log(ageShubham, ageShub);
+
+console.log(ageShubham * 2, ageShub / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 
+
+const firstName = "Shubham";
+const lastName = "Tiwari";
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x+10 = 25
+//console.log(x);
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageShubham > ageShub);
+console.log(ageShub >= 18);
+
+const now = 2037;
+const ageShubham = now - 1991;
+const ageShub = now - 2013;
+
+console.log(now - 1991 > now - 2013);
+
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
