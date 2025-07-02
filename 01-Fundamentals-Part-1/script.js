77d4ede0b5393ce62b7c1c4c04cbe639ae607b31
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-07-03 00:29:49 +0530

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -240,9 +240,22 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); */
 
 //5 falsy value ; 0 , '',undefined ,null , NaN.
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('Jones'));
-console.log(Boolean({}));
-console.log(Boolean(''));
-console.log();
+console.log(Boolean(0)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean('Jones'));//true
+console.log(Boolean({})); //true
+console.log(Boolean('')); //false
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+} // here we get else statement b/c money is 0 and it is falsy
+
+let height;
+if (height) {
+    console.log("YAY! Height is Defined");
+} else {
+    console.log("Height is UNDEFINED");
+} // here code will give else statment b/c height is undefined
