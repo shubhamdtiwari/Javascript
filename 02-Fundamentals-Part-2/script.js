b87680ba81f79c77748b540176bc8526422787fc
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-11-26 15:17:49 +0530

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -4,5 +4,54 @@ let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true; 
-if (hasDriversLicense) console.log('I can drive!') */
+if (hasDriversLicense) console.log('I can drive!') 
+// lecture :- function
+function logger() {
+    console.log('My name is Shubham')
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcesser(apples, oranges) {
+    console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcesser(5, 0);
+
+const appleJuice = fruitProcesser(5, 0);
+console.log(appleJuice);
+
+const appleorangeJuice = fruitProcesser(2, 3);
+console.log(appleorangeJuice); 
+
+// assingment :- 03 function
+function describeCountry(country, population, capitalCity) {
+
+    console.log(country, population, capitalCity)
+    const statement = (`${country} has ${population} million people and its capital city is ${capitalCity}. `);
+
+    return statement;
+}
+describeCountry('Finland', 6, 'Helsinki');
+console.log(describeCountry('Finland', 6, 'Helsinki')) // we need quotes for finland to make it string
+
+const line = describeCountry('Finland', 6, 'Helsinki'); // if we don't use quotes then it will act like variable
+console.log(line);
+const line2 = describeCountry('India', 150, 'New delhi');
+console.log(line2);
+const line3 = describeCountry('China', 120, 'Bejing');
+console.log(line3);
+
+let place = 'USA';
+let City = 'Dc';
+const line4 = describeCountry(place, 60, City);
+console.log(line4) */
+
+// lecture :- 4
+
+function
 
