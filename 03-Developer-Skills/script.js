cca9db39bbd430b4b6dc09ad82649047b6a4034f
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-07 23:10:49 +0530

    Update script.js

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -91,10 +91,17 @@ const calcTempAmplitudeNew = function (t1, t2) {
     if (typeof curTemp !== 'number') continue;
     if (curTemp > max) max = curTemp;
     if (curTemp < min) min = curTemp;
-  }
+  } 
   console.log(max, min);
   return max - min;
 };
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);  */
 // Chanllenge :- 1
+const arr = [17, 21, 23];
+const printForecast = function () {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}°C in ${i + 1} days ...`);
+  }
+};
+printForecast();
