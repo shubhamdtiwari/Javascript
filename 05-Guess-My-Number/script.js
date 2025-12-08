77b1b68b6bc4bf72d7ff9125a110669a1085420f
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-09 00:21:06 +0530

    Update script.js

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -11,7 +11,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -28,7 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.highscore').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscorescore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -59,7 +63,6 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.highscore').textContent = 0;
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = score;
