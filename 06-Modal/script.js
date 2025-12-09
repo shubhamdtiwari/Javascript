9e7548e4400fbfdb2a37d213363ac8980a49dc23
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-09 20:54:45 +0530

    Update script.js

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,11 +1,23 @@
-"use strict";
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnOpenModal);
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 
+// for this we can assign a function
+const OpenModal = function () {
+  // console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// for (let i = 0; i < btnOpenModal.length; i++)
+//   btnOpenModal[i].addEventListener('click', function () {
+//     console.log('Button clicked');
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
 for (let i = 0; i < btnOpenModal.length; i++)
+<<<<<<< HEAD
   btnOpenModal[i].addEventListener("click", function () {
     console.log("Button clicked");
     modal.classList.remove("hidden");
@@ -22,4 +34,32 @@ overlay.addEventListener("click", function () {
   overlay.classList.add('hidden');
 
 
+=======
+  btnOpenModal[i].addEventListener('click', OpenModal);
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+//instead of repeating the code we can assign it to a function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // if (!modal.classList.contains('hidden')) {
+    closeModal();
+    // }
+  }
+>>>>>>> 48ddfa27f0c3325064a92d1a000f277faa70ebc2
 });
