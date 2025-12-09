cb1bce71d1dea7ed8855a4d1227c2533e295e2b9
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-09 18:28:00 +0530

    Update script.js

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,15 +3,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
+// for this we can assign a function
+const OpenModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// for (let i = 0; i < btnOpenModal.length; i++)
+//   btnOpenModal[i].addEventListener('click', function () {
+//     console.log('Button clicked');
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+//   });
 for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
+  btnOpenModal[i].addEventListener('click', OpenModal());
 // btnCloseModal.addEventListener('click', function () {
 //   modal.classList.add('hidden');
 //   overlay.classList.add('hidden');
