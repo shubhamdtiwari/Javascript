1c76e952e32afd727930128ae3457ba3807275af
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-12 00:45:00 +0530

    Update script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  //   console.log(firstName);
+
+  function printAge() {
+    const output = `youu are ${age} , born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+const firstName = "Shubham";
+calcAge(1991);
