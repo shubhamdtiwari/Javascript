b9d3d4531271647574f15631263cc2622ea57cf1
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-12 01:16:29 +0530

    Update script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -6,6 +6,14 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `${firstName} is ${age} , born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `oh! you're a millenial , ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str); it willl show error b/c it is not define in printAge() function
+    console.log(millenial); // but this can acceced outside b/c it is a function scoped, they simply ignore the block as they are not block scoped
   }
   printAge();
   return age;
