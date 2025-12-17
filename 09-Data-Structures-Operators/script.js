895d62b3266254ab9e489d996864a5a02233a7c0
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-17 23:52:59 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -22,14 +22,6 @@
 //   'garlic',
 // ]);
 
-// Data needed for first part of the section
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-};
 //   openingHours: {
 //     thu: {
 //       open: 12,
@@ -45,6 +37,19 @@ const restaurant = {
 //     },
 //   },
 // };
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (staterIndex, mainIndex) {
+    return [this.starterMenu[staterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -52,3 +57,16 @@ const c = arr[3];
 
 const [x, y, z] = arr;
 console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
