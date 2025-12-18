d174fac5144d10ac1eb31daec99391d7adc9a00f
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-18 23:49:48 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -55,9 +55,9 @@ const a = arr[0];
 const b = arr[1];
 const c = arr[3];
 
-// const [x, y, z] = arr;
-// console.log(x, y, z);
-// console.log(arr);
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
 
 // let [main, , secondary] = restaurant.categories;
 // console.log(main, secondary);
