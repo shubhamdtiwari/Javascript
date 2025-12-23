55162425c1efdf27fda0254e0030e0def777992f
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-23 23:56:40 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,41 +1,6 @@
 "use strict";
 
 // Data needed for first part of the section
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto", "Mashroom Masala"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (staterIndex, mainIndex) {
-    return [this.starterMenu[staterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({
-    mainIndex = 0,
-    staterIndex = 1,
-    time = "20:00",
-    address,
-  }) {
-    console.log(
-      `Order recevied! ${this.starterMenu[staterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-};
 
 //lecture : - 05
 
