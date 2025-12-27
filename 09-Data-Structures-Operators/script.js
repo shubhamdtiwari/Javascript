06bc6388bcc8fdfabfd67e980c14bc89cec41104
Shubham Tiwari <shubhamtpandit@gmail.com>
2025-12-27 23:33:02 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto", "Mashroom Masala"],
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto', 'Mashroom Masala'],
   openingHours: {
     thu: {
       open: 12,
@@ -28,7 +28,7 @@ const restaurant = {
   orderDelivery: function ({
     mainIndex = 0,
     staterIndex = 1,
-    time = "20:00",
+    time = '20:00',
     address,
   }) {
     console.log(
@@ -127,10 +127,10 @@ console.log(i, j, k);
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const italianFoods = new Set([
-  "pasta",
-  "gnocchi",
-  "tomatoes",
-  "olive oil",
-  "garlic",
-  "basil",
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
 ]);
