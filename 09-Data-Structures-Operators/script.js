4bb8c3dae129b463214a057d96f012369cceeb61
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-02-19 22:58:00 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -32,7 +32,7 @@ const restaurant = {
     address,
   }) {
     console.log(
-      `Order recevied! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+      `Order recevied! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 };
@@ -48,34 +48,6 @@ restaurant.orderDelivery({
   address: 'Patel Nagar',
   starterIndex: 2,
 });
-// Lec :- 04 Desturcturing objects
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
-
-// seting defult value
-// defult values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
-
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log(a, b);
-
-//nested objects.
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
 
 /*
 
