4c802fb2f26643eb679c5f8dd67bee38509125f0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-06 18:51:29 +0530

    Update script.js

--- 09.1-Data-Structures-Operators Assigment/script.js
+++ 09.1-Data-Structures-Operators Assigment/script.js
@@ -358,3 +358,7 @@ function hasExampleInJava(books) {
 hasExampleInJava(books[0]);
 
 // 5.2
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`"${books[i].title}" provides online content`);
+}
