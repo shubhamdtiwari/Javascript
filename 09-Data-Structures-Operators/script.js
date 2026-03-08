7eda71e050677e6700daf5cd04653ca68a79ba77
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-08 23:32:30 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,44 @@ const restaurant = {
     );
   },
 };
+/*
+
+// lecture :- 09 Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'Shubham',
+  owner: 'Giovani',
+};
+// OR assignment operator
+
+// rest1.numGuests = rest1.numGuests || 10;
+
+// rest1.numGuests ||= 10; // instead of writing long form we can use short form called L.A.O.
+
+// // rest2.numGuests = rest2.numGuests || 10;
+
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+//AND assignment operator
+// rest1.owner = rest1.owner && '<Anonymous>';
+// rest2.owner = rest2.owner && '<Anonymous>';
+
+rest1.owner &&= '<Anonymous>';
+rest2.owner &&= '<Anonymous>';
+
+console.log(rest1);
+console.log(rest2);
 
 //////////////////////////////////
 // lecture :- 08 Nullish Coalescing Operator
