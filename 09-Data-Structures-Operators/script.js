927c7f4b121bc4c4073a40aa9e2e221fb5e6a009
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-12 17:36:02 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -60,8 +60,8 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
 
-for (const item of menu.entries()) {
-  console.log(`${item[0] + 1}: ${item[1]}`);
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
 }
 
 // console.log([...menu.entries()]);
