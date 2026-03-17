ad825308cec1e8fba97ccac110d12aeb0def3320
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-18 00:00:14 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,3 +1,67 @@
+'use strict';
+
+// Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto', 'Mashroom Masala'],
+  //here we can insert object directly but variable name is same for the object
+  // openingHours: openingHours,
+
+  //ES6 enhanced object literals
+  openingHours,
+
+  // here fuction syntax can be removed to make the code simple
+
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  // same for this
+  orderDelivery({ mainIndex = 1, starterIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order recevied! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1} , ${ing2} and ${ing3}`,
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+    const taste = otherIngredients.length === 0 ? 'bad' : 'delicious';
+    const topping =
+      otherIngredients.length === 0 ? 'none' : otherIngredients.join(',');
+    console.log(
+      `Main ingredient in this pizza is made of ${mainIngredient} and topings are made up of ${topping}. and it's looks ${taste}`,
+    );
+  },
+};
+
 /* 
 // lecture :- 14 Looping Objects
 ////////
