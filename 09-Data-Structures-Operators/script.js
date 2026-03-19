ad99fbc45f1900d37cda48151187a6cfe256d22a
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-19 23:38:34 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -91,6 +91,7 @@ const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
+console.log(new Set(staff).size);
 /*
 // lecture :- 14 Looping Objects
 ////////
