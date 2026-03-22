c1913c372e4bdc4b1e879c17dbdb0c554e5bee71
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-23 02:05:40 +0530

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -78,6 +78,27 @@ console.log(passengerCorrect);
 
 // Comapirng email
 
+const email = 'hello@jonas.io';
+const loginEmail = '   Hello@Jonas.Io \n';
+
+// const lowereEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowereEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+//replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passenger come to boarding door 23. Boarding door 23';
+
+console.log(announcement.replace('door', 'gate'));
+
+console.log(announcement.replaceAll('door', 'gate'));
+
 /*
 ///////////////////////////////////////////
 //Lecture :- Working with Strings - part 1
