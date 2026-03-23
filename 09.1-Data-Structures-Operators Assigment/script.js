384907951a2d2abc289d351c6cf0f6d4d8089bc0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-24 02:17:05 +0530

    Update script.js

--- 09.1-Data-Structures-Operators Assigment/script.js
+++ 09.1-Data-Structures-Operators Assigment/script.js
@@ -909,35 +909,34 @@ for (const [key, value] of gameEvents) {
 Write a program that receives a list of variable names written in underscore_case 
 and convert them to camelCase. 
 The input will come from a textarea inserted into the DOM (see code below to 
-insert the elements), and conversion will happen when the button is pressed. 
+insert the elements), and conversion will happen when the button is pressed.
+
+
 Test data (pasted to textarea, including spaces): 
 underscore_case 
 first_name 
 Some_Variable  
 calculate_AGE 
 delayed_departure 
+
+
 Should produce this output (5 separate console.log outputs): 
-underscoreCase      
-✅ 
-firstName           
-✅✅ 
-someVariable        
-✅✅✅ 
-calculateAge        
-✅✅✅✅ 
-delayedDeparture    
-✅✅✅✅✅ 
+underscoreCase   ✅ 
+firstName        ✅✅ 
+someVariable     ✅✅✅ 
+calculateAge     ✅✅✅✅ 
+delayedDeparture ✅✅✅✅✅ 
+
+
 Hints: 
 § Remember which character defines a new line in the textarea 
-�
-� 
 § The solution only needs to work for a variable made out of 2 words, like a_b 
 § Start without worrying about the 
 name conversion working 
-✅. Tackle that only after you have the variable 
-�
-� 
+✅. Tackle that only after you have the variable
 § This challenge is difficult on purpose, so start watching the solution in case 
 you're stuck. Then pause and continue! 
+
+
 Afterwards, test with your own test data! 
 */
