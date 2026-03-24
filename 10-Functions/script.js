c6f1c306fa43d035c60263590f3f29ceb369c06e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-25 01:30:12 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,18 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //ES6
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
