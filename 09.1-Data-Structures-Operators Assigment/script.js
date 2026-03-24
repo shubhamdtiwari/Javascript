573d01159335a35fe35361b27cfc86454cc611c0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-25 01:14:14 +0530

    Update script.js

--- 09.1-Data-Structures-Operators Assigment/script.js
+++ 09.1-Data-Structures-Operators Assigment/script.js
@@ -1003,10 +1003,17 @@ document.querySelector('button').addEventListener('click', function () {
   const rows = text.split('\n');
   console.log(rows);
 
-  for (const row of rows) {
+  for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split('_');
 
     const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
-    console.log(output);
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+
+///////////////////////
+
+// Coding Challenge
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
