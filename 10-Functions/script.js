dae4078bab8152c0034653657de6597ff7a4c7b8
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-26 00:50:24 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -124,3 +124,14 @@ const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
 };
+
+// Higher-order function
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
