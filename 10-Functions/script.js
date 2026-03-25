4651e5e0b70936363964fe438ae077dc34855a46
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-25 23:59:17 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -101,3 +101,15 @@ const counter = {
 // function that recives another function as an argument that returns a new function
 
 // Example
+const greet = () => console.log('Hey Shubham');
+btnClose: addEventListener('click', greet);
+
+// function that returns new function
+function count() {
+  // upper function is a higher order function
+  let counter = 0;
+  return function () {
+    // this is returned function
+    counter++;
+  };
+}
