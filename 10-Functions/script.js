49616f7a8a3e5d9612d8a0612f7c40bbb1cb4c10
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-27 17:47:02 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -271,3 +271,13 @@ const addVAT = addTax.bind(null, 0.23);
 // addVAT = (rate, value) => value + value * 0.23
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const AddVAT2 = addTaxRate(0.23);
+
+console.log(AddVAT2(100));
