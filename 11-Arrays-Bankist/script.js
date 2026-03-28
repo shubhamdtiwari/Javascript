2e62b09388ae7787b51f8bb6a74a9ab8dfd7450d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-29 01:19:11 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -130,19 +130,24 @@ console.log('shubham'.at(-1));
 */
 // lecture :- Looping Arrays: FOREACH
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-for (const movement of movements) {
+
+for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You Deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You Deposited ${movement}`);
   } else {
-    console.log(`You withdraw ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(movement)}`);
   }
 }
 
 console.log('---- FOREACH ----');
-movements.forEach(function (movement) {
-  if (movement > 0) {
-    console.log(`You Deposited ${movement}`);
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You Deposited ${mov}`);
   } else {
-    console.log(`You withdraw ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdraw ${Math.abs(mov)}`);
   }
 });
+// 0: function(200)
+// 1: function(450)
+// 3: function(400)
+// ...
