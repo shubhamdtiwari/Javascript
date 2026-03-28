3375d4bc4971896327ea5ed42fd7c5503f3c0c96
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-28 12:14:12 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -438,7 +438,7 @@ booker();
 // A closure gives a function access to all the variables of its parent function, even after that parent function has returned. the function keeps a refernce to its outer scope, which preserve the scope chain throughout time
 
 console.dir(booker);
-*/
+
 
 // Lecture :- examples of Clouser
 
@@ -478,5 +478,26 @@ const boardPassengers = function (n, wait) {
 
   console.log(`Will start boarding in ${wait} second`);
 };
-
+const perGroup = 1000;
 boardPassengers(180, 3);
+
+
+
+/////////////////////////////
+
+// Coding Challenge #2
+
+This is more of a thinking challenge than a coding challenge 
+Your tasks: 
+1. Take the IIFE below and at the end of the function, attach an event listener that 
+changes the color of the selected h1 element ('header') to blue, each time 
+the body element is clicked. Do not select the h1 element again! 
+2. And now explain to yourself (or someone around you) why this worked! Take all 
+the time you need. Think about when exactly the callback function is executed, 
+and what that means for the variables involved in this example.
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+})();
