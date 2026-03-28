5d6e7394ff37bb8bee3d0b6793b392f2d8e49529
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-28 11:54:04 +0530

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -441,3 +441,26 @@ console.dir(booker);
 */
 
 // Lecture :- examples of Clouser
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+
+//Re-assigning f function
+h();
+f();
+console.dir(f);
