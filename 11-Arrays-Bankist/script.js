bb2f6ea89a4d6550f07a7e72fa33e95739dae77a
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-29 16:32:12 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -167,6 +167,7 @@ for (const [i, movement] of movements.entries()) {
 }
 
 console.log('---- FOREACH ----');
+
 movements.forEach(function (mov, i, arr) {
   if (mov > 0) {
     console.log(`Movement ${i + 1}: You Deposited ${mov}`);
@@ -201,7 +202,7 @@ currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
 
-/////////////
+/////////////////////////////////
 // Chanllenge :- 1
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
 about their dog's age, and stored the data into an array (one array for each). For 
@@ -209,15 +210,20 @@ now, they are just interested in knowing whether a dog is an adult or a puppy.
 A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
 old. 
 Your tasks: 
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
-('dogsJulia' and 'dogsKate'), and does the following things: 
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things: 
+
 1. Julia found out that the owners of the first and the last two dogs actually have 
 cats, not dogs! So create a shallow copy of Julia's array, and remove the cat 
 ages from that copied array (because it's a bad practice to mutate function 
 parameters) 
+
+
 2. Create an array with both Julia's (corrected) and Kate's data 
+
 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy )
+
 4. Run the function for both test datasets 
 Test data: 
 ") 
@@ -226,3 +232,29 @@ Test data:
 Hints: Use tools from all lectures in this section so far
 
 */
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  //1.
+
+  const shallowDogsJulia = dogsJulia.slice(1, 3);
+
+  //2.
+  const joinDogs = shallowDogsJulia.concat(dogsKate);
+
+  //3.
+
+  joinDogs.forEach(function (dogs, i, arr) {
+    if (dogs >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dogs} years old`);
+    } else {
+      console.log(
+        `Dog number ${i + 1} is still a puppy🐶 and ${dogs} years old`,
+      );
+    }
+  });
+};
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+checkDogs(dogsJulia, dogsKate);
