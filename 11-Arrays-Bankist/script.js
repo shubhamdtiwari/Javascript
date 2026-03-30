e5e08bfc41550a094ba15a8fda3629f9a10e435d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-30 23:44:41 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -128,6 +128,19 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value,
+  );
+  console.log(currentAccount);
+});
+
 // console.log(containerMovements.innerHTML);
 // this shows the html created for the incoming values
 
