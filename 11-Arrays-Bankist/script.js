20f87a79b89bb7b12648e275646b74ade12b2d9d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-03-31 12:15:33 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -193,6 +193,20 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username,
+    );
+
+    accounts.splice(index, 1);
+  }
+});
 // console.log(containerMovements.innerHTML);
 // this shows the html created for the incoming values
 
