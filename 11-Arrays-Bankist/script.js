38c38c0ae3545737f053b12f2b1cee048c50590b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-01 23:56:28 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -685,4 +685,35 @@ recommended portion, and eating too little is the opposite.
 Eating an okay amount means the dog's current food portion is within a range 10% 
 above and 10% below the recommended portion (see hint).
 
+
+Your tasks:
+
+
+1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate 
+the recommended food portion and add it to the object as a new property. Do 
+not create a new array, simply loop over the array. Forumla: 
+recommendedFood = weight ** 0.75 * 28. (The result is in grams of 
+food, and the weight needs to be in kg) 
+2. Find Sarah's dog and log to the console whether it's eating too much or too 
+little. Hint: Some dogs have multiple owners, so you first need to find Sarah in 
+the owners array, and so this one is a bit tricky (on purpose) 
+�
+� 
+3. Create an array containing all owners of dogs who eat too much 
+('ownersEatTooMuch') and an array with all owners of dogs who eat too little 
+('ownersEatTooLittle'). 
+4. Log a string to the console for each array created in 3., like this: "Matilda and 
+Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat 
+too little!" 
+5. Log to the console whether there is any dog eating exactly the amount of food 
+that is recommended (just true or false) 
+6. Log to the console whether there is any dog eating an okay amount of food 
+(just true or false) 
+
+7. Create an array containing the dogs that are eating an okay amount of food (try 
+to reuse the condition used in 6.) 
+
+8. Create a shallow copy of the 'dogs' array and sort it by recommended food 
+portion in an ascending order (keep in mind that the portions are inside the 
+array's objects ) 
 */
