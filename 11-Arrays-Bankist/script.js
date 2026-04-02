9244d494b62634120eaa36a8de45ff6016075289
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-02 18:38:30 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -626,7 +626,7 @@ console.log(groupedAccounts);
 */
 // more ways of creating and filling arrays
 
-console.log([1, 2, 3, 4, 5, 6, 7]);
+const arr = [1, 2, 3, 4, 5, 6, 7];
 
 console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
@@ -634,6 +634,22 @@ console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
 const x = new Array(7);
 console.log(x);
+console.log(x.map(() => 5)); // it does not work
+
+// x.fill(1);
+
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (cur, i) => i + 1);
+console.log(z);
 
 /*
 ///////////////////////////////////////////////
