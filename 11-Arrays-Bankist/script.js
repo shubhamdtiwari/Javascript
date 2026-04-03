2c2b5e40bc6a8543aae4f2eadd6d06eeed3b1751
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-03 23:57:40 +0530

    Update script.js

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -255,6 +255,14 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 //////////////////////////////////////
 //////////////////////////////////////////////
 // Lecture :- Simple Array Methods
