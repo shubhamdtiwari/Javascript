c325f4f9cf11ffc1d25f248a73f49df8f6425535
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-04 16:37:09 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -293,7 +293,7 @@ console.log(Number.isFinite(23 / 0)); //false
 
 console.log(Number.isInteger(23)); //true
 console.log(Number.isInteger(23.0)); //true
-*/
+
 
 // Lecture :- Math and ronding
 
@@ -340,3 +340,11 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+*/
+
+// Remainder oprator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
