22ba2a175fa3f337aef9476ecfd8441985ff55b4
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-05 23:56:36 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -93,6 +93,17 @@ const displayMovements = function (acc, sort = false) {
 
     acc.movementsDates[i];
 
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}  </div>
+    <div class="movements__date">${displayDate}</div>
+  
+        <div class="movements__value">${mov.toFixed(2)}€</div>
+      </div>
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
