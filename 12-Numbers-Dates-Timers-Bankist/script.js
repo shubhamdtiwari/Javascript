de472efb5c58a3b9337424607f4ab6d2c25ea39c
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-07 02:23:18 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -532,7 +532,7 @@ const future = new Date(2037, 10, 19, 15, 23, 5);
 console.log(+future);
 
 const calcDaysPassed = (date1, date2) =>
-  (date2 - date1) / (1000 * 60 * 60 * 24);
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 
-const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
