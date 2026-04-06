76d5ff4c62a8f37e3168ad5c6f8a101599a77edf
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-07 02:57:15 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -198,16 +198,16 @@ labelWelcome.textContent = `Welcome back, ${
   currentAccount.owner.split(' ')[0]
 }`;
 
+// Experimenting API
 const now = new Date();
-const day = `${now.getDate()}`.padStart(2, 0);
-const month = `${now.getMonth() + 1}`.padStart(2, 0); // b/c it is zero based
-const year = now.getFullYear();
-const hour = now.getHours();
-const min = now.getMinutes();
-
-labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
-
-// day/month/year
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+};
+labelDate.textContent = new Intl.DateTimeFormat('en-GB', options).format(now);
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
