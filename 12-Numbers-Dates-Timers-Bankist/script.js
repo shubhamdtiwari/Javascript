d12287c3ea43e26eee512d69f5cfc384c973a35e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-07 23:46:34 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -599,6 +599,7 @@ console.log(
 
 // Lecture :- Timers: settimeout and setinterval
 
+//setTimeout
 const ingredients = ['olives', 'spinach'];
 const pizzaTimer = setTimeout(
   (ing1, ing2) => console.log(`Here is your Pizza🍕 with ${ing1} and ${ing2}`),
@@ -609,3 +610,12 @@ const pizzaTimer = setTimeout(
 console.log('Waiting...');
 
 if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(() => {
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+  console.log(`${hour}:${min}:${sec}`);
+}, 1000);
