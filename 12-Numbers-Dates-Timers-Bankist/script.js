aae1d7c1bf3c0d9b79b326b767938e99574cfc3a
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-07 23:55:15 +0530

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -198,6 +198,16 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  //set time to 5 min
+
+  // call the timer every sec
+  setInterval(() => {
+    // in each call , print the remaining time to UI
+    // When 0 sec , stop timer and log out user
+  }, 1000);
+};
+
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
