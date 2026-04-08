0417effd05a52dccb55c7fada48e0bf9c583deab
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-08 23:33:30 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -31,15 +31,3 @@ document.addEventListener('keydown', function (e) {
 });
 /////////////////////////////
 // Lecture :- Selecting , Creating and Deleting an Element
-
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
-
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
