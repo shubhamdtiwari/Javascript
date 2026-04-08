9ed89e47f4d74c7330ef59fb865d16ce951d4227
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-08 23:32:15 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -39,3 +39,7 @@ console.log(document.body);
 document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
