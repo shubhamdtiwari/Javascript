a84685435f9f90a469d0902bb61598219adbdb1d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-09 23:57:01 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -3,17 +3,6 @@
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
