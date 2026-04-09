a585b70ec9bc88f40dc3c7619604e87ce5273350
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-09 19:27:45 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -98,9 +98,20 @@ document.documentElement.style.setProperty('--color-primary', 'orangered');
 // attributes
 
 const logo = document.querySelector('.nav__logo');
-console.log(logo.src);
 console.log(logo.alt);
 console.log(logo.className);
 
+logo.alt = 'Beatiful minimalist logo';
+
 // Non-Standard
 console.log(logo.designer); // it gies undefined as it is not a property of images
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
