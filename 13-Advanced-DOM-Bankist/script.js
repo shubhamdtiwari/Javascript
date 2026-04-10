462943748411040ffd3b95066105302c86278159
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-10 23:43:34 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -19,6 +19,16 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 /////////////////////////////
 
 /*
