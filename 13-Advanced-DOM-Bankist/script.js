2dcb7f81ef1eec6430e468d4154784039aee9f3b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-10 22:49:31 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -137,4 +137,9 @@ logo.className = 'jonas';
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 
-const section! = document.querySelector('#section--1')
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+});
