4f97e1c66a8de722d8a0d15eb265575dc3b2893b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-10 23:47:11 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -142,4 +142,6 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
 });
