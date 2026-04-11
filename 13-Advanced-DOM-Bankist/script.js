8468c95d98bf340b305a8be2132dc0f9d939bef0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-11 23:09:04 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -148,14 +148,14 @@ btnScrollTo.addEventListener('click', function (e) {
 
   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll(X/Y', window.pageXOffset, pageYOffset);
+  console.log('Current scroll(X/Y', window.scrollX, scrollY);
 
-  console.log(
-    'height/wigth viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth,
-  );
+  // console.log(
+  //   'height/wigth viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth,
+  // );
 
-  // Scrolling
-  window.scrollTo(s1coords.left, s1coords.right);
+  // // Scrolling
+  // window.scrollTo(s1coords.left, s1coords.right);
 });
