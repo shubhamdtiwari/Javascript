a240505a2665b99d51b95f1864b725ab22aae7c7
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-11 23:15:54 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -148,13 +148,14 @@ btnScrollTo.addEventListener('click', function (e) {
 
   console.log(e.target.getBoundingClientRect());
 
+  // it is use to show the coordinates b/w the top of web browser
   console.log('Current scroll(X/Y', window.scrollX, scrollY);
 
-  // console.log(
-  //   'height/wigth viewport',
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth,
-  // );
+  console.log(
+    'height/wigth viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth,
+  );
 
   // // Scrolling
   // window.scrollTo(s1coords.left, s1coords.right);
