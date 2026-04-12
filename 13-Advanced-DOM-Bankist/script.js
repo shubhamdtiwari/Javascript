da438cfb48205b100f056f2b25f2ff666d440c28
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-12 23:46:40 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -179,16 +179,6 @@ btnScrollTo.addEventListener('click', function (e) {
 
 // Lecture :- Types of Events and Event Handlers
 
-const h1 = document.querySelector('h1');
-
-const alertH1 = function (e) {
-  alert('addEventListener: Great! You are reading the heading :D ');
-
-  // it use to remove the eventListerner
-  // we can use it anywhere
-  // h1.removeEventListener('mouseenter', alertH1);
-};
-
 h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
