5c2057670ba421335c917fdaae10299baf47086b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-12 23:45:25 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -188,3 +188,11 @@ const alertH1 = function (e) {
   // we can use it anywhere
   // h1.removeEventListener('mouseenter', alertH1);
 };
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
