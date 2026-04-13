a5a7517f6d3a3d26ca87f2dddb2bfbbd8de8e904
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-14 00:49:25 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -65,6 +65,13 @@ btnScrollTo.addEventListener('click', function (e) {
 //   });
 // });
 
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the  event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+});
+
 ////////////////////////
 ////////////////////////////////
 /////////////////////////////////////////
