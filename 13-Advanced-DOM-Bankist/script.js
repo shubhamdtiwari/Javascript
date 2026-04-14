8c7ca4f6fcaa664b6c5d759ccf82c95f65249855
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-14 23:58:00 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -71,13 +71,7 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
   // Matching strategy
 
-  if (e.target.classList.contains('nav__link')) {
-    e.preventDefault();
-    const id = e.target.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  }
-});
+
 
 ////////////////////////
 ////////////////////////////////
