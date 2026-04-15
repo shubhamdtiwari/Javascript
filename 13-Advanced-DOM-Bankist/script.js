c2e62de269a3de6dd8b0eb8cd4116606f25b715e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-15 23:39:07 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -32,24 +32,6 @@ document.addEventListener('keydown', function (e) {
 });
 /////////////////////////////
 // Button Scrolling
-btnScrollTo.addEventListener('click', function (e) {
-  // to get the coordinate to which we have to go
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-
-  console.log(e.target.getBoundingClientRect());
-
-  // it is use to show the coordinates b/w the top of web browser
-  console.log('Current scroll(X/Y', window.scrollX, window.scrollY);
-
-  console.log(
-    'height/wigth viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth,
-  );
-
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
 
 ////////////////////////////
 // Page nagivation
