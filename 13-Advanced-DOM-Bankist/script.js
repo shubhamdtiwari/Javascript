2ff2b9cf96eb5ec1b06f3aaa090fc51989c1e631
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-16 23:46:31 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -115,31 +115,22 @@ tabsContainer.addEventListener('click', function (e) {
 
 // Menu fade animation
 
-nav.addEventListener('mouseover', function (e) {
+const HandleHover = function (e, opacity) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 0.5;
+      if (el !== link) el.style.opacity = opacity;
     });
-    logo.style.opacity = 0.5;
+    logo.style.opacity = opacity;
   }
-});
+};
 
-nav.addEventListener('mouseout', function (e) {
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('nav__link');
-    const logo = link.closest('.nav').querySelector('img');
+nav.addEventListener('mouseover', HandleHover);
 
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 1;
-    });
-    logo.style.opacity = 1;
-  }
-});
+nav.addEventListener('mouseout', HandleHover);
 
 ////////////////////////
 ////////////////////////////////
