7f41241fa1b8fbe4cb3ee40318288af46e9b6ef3
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-17 00:02:57 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -134,8 +134,14 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
 window.addEventListener('scroll', function (e) {
   console.log(window.scrollY);
+
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 });
 
 ////////////////////////
