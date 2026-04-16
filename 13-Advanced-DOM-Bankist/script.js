fbc84202ca0bfa488c7610af16f12c20b01e0a1a
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-17 02:31:25 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -147,7 +147,11 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation: Intersection Observer API
 
-const obsCallback = function () {};
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+};
 
 const obsOptions = {
   root: null,
