1e20ab7a1cadc1f6831926844e9ae9707dd993dd
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-16 23:42:24 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -118,7 +118,7 @@ tabsContainer.addEventListener('click', function (e) {
 nav.addEventListener('mouseover', function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('nav__link');
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach((el) => {
