823350655fa40280a3eab111ea4485e0c49b9707
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-17 02:51:07 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -166,7 +166,9 @@ const header = document.querySelector('.header');
 const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
-  nav.classList.remove('sticky');
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
