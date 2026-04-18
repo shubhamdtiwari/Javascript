9db495a7eaeeef5ba6878e9d6fab8257a248d0a8
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-18 23:10:54 +0530

    Add modal and overlay elements to script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -3,6 +3,9 @@
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 
 const tabs = document.querySelectorAll('.operations__tab');
