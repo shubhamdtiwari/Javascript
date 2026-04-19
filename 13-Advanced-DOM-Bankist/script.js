7bd66e23f594e8f7cfdda221126ae516d1579a55
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-19 09:04:13 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -181,9 +181,13 @@ const allSections = document.querySelectorAll('.section');
 
 const revelSection = function (entries, observer) {};
 
-const sectionObserver = new IntersectionObserver(revelSection, {});
+const sectionObserver = new IntersectionObserver(revelSection, {
+  root: null,
+  threshold: 0.15,
+});
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
+  section.classList.add('section--hidden');
 });
 
 ////////////////////////
