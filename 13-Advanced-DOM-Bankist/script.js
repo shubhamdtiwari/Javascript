143fe65091497f3a6039f585f805405567e5f220
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-04-22 23:59:50 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -217,6 +217,10 @@ const loadImg = function (entries, observer) {
 
   //Replace src with data-src
   entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
