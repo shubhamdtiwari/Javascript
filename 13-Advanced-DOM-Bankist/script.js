59141eaf7ae8409908780961ec79ec34f0804d7b
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-07 14:20:00 +0530

    Update script.js

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -241,6 +241,7 @@ const slides = document.querySelectorAll('.slide');
 
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 const maxSlide = slides.length;
