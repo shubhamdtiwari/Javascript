5da4abc7703d5a104980b9fba2ac2c9aced9f1b7
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-09 18:13:03 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,12 +1,30 @@
 'use strict';
 
+// Lecture :- Constructor Function and the new Operator
+
 const Person = function (firstName, birthYear) {
-  console.log(this);
+  //   console.log(this);
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // this not be done
+  // we should never create a method inside a constructor
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
-new Person('jonas', 1991);
+const jonas = new Person('jonas', 1991);
+console.log(jonas);
 
 // 1. New {} is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('jack', 1990);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); // true as we create object
