ac403f89b0f0159029e3e204684f718ce96a4a31
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-09 23:36:57 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -73,3 +73,8 @@ console.log(jonas.__proto__.__proto__.__proto__);
 console.dir(Person.prototype.constructor);
 
 const arr = [3, 6, 4, 5, 6, 8];
+
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log();
