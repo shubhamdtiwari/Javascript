a5fa1f50d1556bf063d41b84bd404fa437048254
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-09 18:42:17 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -18,7 +18,7 @@ const Person = function (firstName, birthYear) {
 const jonas = new Person('jonas', 1991);
 console.log(jonas);
 
-// 1. New {} is created
+// 1. New {} object is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
@@ -53,3 +53,11 @@ console.log(Person.prototype.isPrototypeOf(jonas));
 console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(Person.prototype.isPrototypeOf(Person));
 // .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas, matilda);
+
+console.log(jonas.hasOwnProperty('firstName'));
+
+console.log(jonas.hasOwnProperty('species'));
+// false as it this not a property inside the jonas object. It simply has access due to its prototype property in person
