1ed3184fbe7292a22a9c0769a0192bf8a0899ddf
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-10 12:42:59 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Lecture :- Constructor Function and the new Operator
-
+/*
 const Person = function (firstName, birthYear) {
   //   console.log(this);
   // Instance properties
@@ -88,8 +88,7 @@ console.log(arr.unique());
 // Coding Challenge #1
 /*
 Your tasks: 
-1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
-'speed' property. The 'speed' property is the current speed of the car in 
+1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 'speed' property. The 'speed' property is the current speed of the car in 
 km/h 
 2. Implement an 'accelerate' method that will increase the car's speed by 10, 
 and log the new speed to the console 
@@ -100,3 +99,10 @@ the new speed to the console
 Test data: 
 § Data car 1: 'BMW' going at 120 km/h 
 § Data car 2: 'Mercedes' going at 95 km/h */
+
+// 1.
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
