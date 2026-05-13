d8c61652251d5561e56e94bf0d9007cf1319e6d3
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-13 18:46:10 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -400,7 +400,6 @@ const EV = function (make, speed, charge) {
   Car.call(this, make, speed);
 
   this.charge = this.charge;
-
 };
 
 EV.prototype = Object.create(Car.prototype);
@@ -409,13 +408,17 @@ EV.prototype.chargeBattery = function (chargeTo) {
   this.charge = chargeTo;
 };
 
-EV.prototype.accelerate = function() {
+EV.prototype.accelerate = function () {
   this.speed += 20;
   this.charge--;
 
-  console.log(`${this.}`);
-}
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
 
 const tesla = new EV('Tesla', 120, 23);
 tesla.chargeBattery(90);
 console.log(tesla);
+tesla.break();
+tesla.accelerate();
