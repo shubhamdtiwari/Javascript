527418b0b74213aad886ae64aa0a68d6b65ca618
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-14 19:53:56 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -539,12 +539,24 @@ class Account {
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // Public interface
   deposit(val) {
     this.movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
   }
 }
 
@@ -553,4 +565,8 @@ const acc1 = new Account('Jonas', 'EUR', 1111, []);
 // acc1.movements.push(250);
 // acc1.movements.push(-140);
 
+acc1.deposit(250);
+acc1.withdraw(140);
+
 console.log(acc1);
+console.log(acc1.pin);
