47dac33d5d5adb221fdf4c3696f5b11373e47fcb
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-16 00:25:11 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -673,45 +673,29 @@ console.log(movements);
 
 // § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
 
-
 */
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+class Carcl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-Car.prototype.break = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
 
-const EV = function (make, speed, charge) {
-  Car.call(this, make, speed);
+  break() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
 
-  this.charge = this.charge;
-};
+  get speedUS() {
+    return this.speed / 1.6;
+  }
 
-EV.prototype = Object.create(Car.prototype);
-
-EV.prototype.chargeBattery = function (chargeTo) {
-  this.charge = chargeTo;
-};
-EV.prototype.accelerate = function () {
-  this.speed += 20;
-  this.charge--;
-
-  console.log(
-    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`,
-  );
-};
-
-const tesla = new EV('Tesla', 120, 23);
-tesla.chargeBattery(90);
-console.log(tesla);
-tesla.break();
-tesla.accelerate();
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
