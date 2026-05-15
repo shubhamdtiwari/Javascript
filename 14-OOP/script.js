02676c07b62ac0a6a4c2144501ecb9ec494107d1
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-15 23:52:19 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -675,3 +675,13 @@ console.log(movements);
 
 
 */
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
