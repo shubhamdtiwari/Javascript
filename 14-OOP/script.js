a61dac38f1ea20709204378ed3ddf5a7db690141
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-17 00:21:22 +0530

    Update script.js

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -701,10 +701,31 @@ class Carcl {
   }
 }
 
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+
+  this.charge = this.charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`,
+  );
+};
+
 class EVCL extends Carcl {
   constructor(make, speed) {
     super(make, speed);
-    this.make = make;
-    this.speed = speed;
   }
 }
+
+const car1 = new EVCL();
