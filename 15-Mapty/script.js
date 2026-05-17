1c4ce177d56171e0dd4cadac9ae6992f49226252
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-17 19:38:49 +0530

    Update script.js
    adding map code

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -16,8 +16,21 @@ if (navigator.geolocation)
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
+      console.log(latitude, longitude);
 
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not not your position');
