94b5f755f305f7d7b0b52e5928ce8d48c5a681d0
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-22 01:14:56 +0530

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -6,6 +6,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; // [lay, lng]
@@ -19,6 +20,10 @@ class Workout {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -282,6 +287,9 @@ class App {
         duration: 1,
       },
     });
+
+    // using the public interface
+    workout.click();
   }
 }
 
