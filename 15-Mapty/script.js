efe11d19e95e70c6af0d58bc741b5a65ddac921d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-21 18:09:24 +0530

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -203,7 +203,7 @@ class App {
   }
 
   _renderWorkout(workout) {
-    const html = `
+    let html = `
     <li class="workout workout--${workout.type}" data-id="${workout.id}">
           <h2 class="workout__title">${workout.description}</h2>
           <div class="workout__details">
@@ -221,15 +221,30 @@ class App {
       html += `
      <div class="workout__details">
             <span class="workout__icon">⚡️</span>
-            <span class="workout__value">4.6</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
             <span class="workout__unit">min/km</span>
           </div>
           <div class="workout__details">
             <span class="workout__icon">🦶🏼</span>
-            <span class="workout__value">178</span>
+            <span class="workout__value">${workout.cadence}</span>
             <span class="workout__unit">spm</span>
           </div>
         </li>`;
+
+    if (workout.type === 'cycling')
+      html += `
+        <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li> 
+    `;
   }
 }
 
