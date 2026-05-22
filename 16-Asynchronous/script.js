195a2bf36d9deddd839333e46b45bf2c708aa586
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-22 11:57:35 +0530

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -10,28 +10,35 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  //   console.log(this.responseText);
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/name/india');
-request.send();
-console.log(this.responseText);
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
 
-request.addEventListener('load', function () {
-  console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-
-  const html = `
+    const html = `
   <article class="country">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} mil. people</p>
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
           </div>
         </article>
         `;
-});
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('bharat');
+getCountryData('usa');
