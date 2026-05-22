040a5dbad577ced34e2996171816496527d675e6
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-22 13:44:16 +0530

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -48,7 +48,23 @@ getCountryData('china');
 */
 
 // Lecture :- callback
-const
+const renderCountry = function (data) {
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} mil. people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 const getCountrAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
@@ -61,6 +77,5 @@ const getCountrAndNeighbour = function (country) {
 
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-
-
+  });
 };
