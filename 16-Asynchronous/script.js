5afa1b46b69bbbcde7595e3d628a282f0d3e8946
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-23 15:34:17 +0530

    catching errors

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -254,10 +254,17 @@ const whereAmI = function (lat, lng) {
   fetch(
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
   )
-    .then((response) => response.json())
+    .then((res) => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+
+      return res.json();
+    })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       console.log(`You are in ${data.city}, ${data.countryName}`);
+    })
+    .catch((err) => {
+      console.error(`${err.message}💥`);
     });
 };
 
