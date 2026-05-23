6c458cb5d780a2e56c21586ca2b254279bb1f710
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-23 12:27:37 +0530

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -143,7 +143,11 @@ const getCountryData = function (country) {
       // Country 2
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
     .then((data) => renderCountry(data, 'neighbour'))
     .catch((err) => {
       console.error(`${err} 💥💥💥`);
