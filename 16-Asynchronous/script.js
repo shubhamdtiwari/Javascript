664043ba40f29e641b809b220fe6f2b9b4f0ddc7
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-24 15:37:18 +0530

    Coding challenge :- 2

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -365,7 +365,7 @@ wait(1)
 
 Promise.resolve('abc').then((x) => console.log(x));
 Promise.reject(new Error('Problem!')).catch((x) => console.log(x));
-*/
+
 // navigator.geolocation.getCurrentPosition(
 //   (position) => console.log(position),
 //   (err) => console.err(err),
@@ -421,3 +421,33 @@ const whereAmI = function () {
 };
 
 btn.addEventListener('click', whereAmI);
+
+
+// Coding Challenge :-
+
+Your tasks: 
+
+PART 1 
+1. Create a function 'createImage' which receives 'imgPath' as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path 
+
+2. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image (listen for the'error' event), reject the promise 
+
+3. If this part is too tricky for you, just watch the first part of the solution 
+
+
+PART 2 
+4. Consume the promise using .then and also add an error handler 
+
+5. After the image has loaded, pause execution for 2 seconds using the 'wait' function we created earlier 
+
+6. After the 2 seconds have passed, hide the current image (set display CSS property to 'none'), and load a second image (Hint: Use the image element returned by the 'createImage' promise to hide the current image. You will need a global variable for that)
+
+7. After the second image has loaded, pause execution for 2 seconds again 
+
+8. After the 2 seconds have passed, hide the current image 
+
+
+Test data: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to “Fast 3G” in the dev tools Network tab, otherwise images load too fast 
+
+
+*/
