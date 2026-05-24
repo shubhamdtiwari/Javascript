24b9c50d710ce52afcd3f0913c527712ee76e55e
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-24 15:55:27 +0530

    Part 1

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -451,3 +451,23 @@ Test data: Images in the img folder. Test the error handler by passing a wrong i
 
 
 */
+
+//1.
+
+const imgContainer = document.querySelector('.image');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
