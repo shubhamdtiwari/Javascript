39c5d941b5864b17d38b3d8bc210dad663628db9
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-25 23:54:51 +0530

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -549,41 +549,30 @@ while (dice !== 6) {
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log('Loop is about to end;');
 } 
+    */
 // Challenge :-  4
-const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 
 for (let i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i]);
-    tips.push(tip);
-    totals.push(bills[i] + tip);
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
 }
 
 console.log(bills, tips, totals);
 
 const calcAvgrage = function (arr) {
-    let sum = onabort;
-    for (let i = 0; i < arr.length; i++) {
-        // sum = sum + arr[i];
-        sum += arr[i]
-    }
-    return sum / arr.length;
-}
+  let sum = onabort;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
 console.log(calcAvgrage(totals));
 console.log(calcAvgrage(tips));
-*/
-var createCounter = function (n) {
-  let x = -1;
-  return function () {
-    x++;
-    return n + x;
-  };
-};
-
-const counter = createCounter(10);
-counter(); // 10
-counter(); // 11
-counter(); // 12
