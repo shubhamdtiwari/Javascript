691b20170c4e4f393579b5570cd907e9fb0a59b9
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-27 00:48:38 +0530

    export

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,5 +1,10 @@
 // Expoting module
-console.log('exporting module');
+console.log('Exporting module');
 
 const shippingCost = 10;
 const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
