f398450cf8061783c577a19b7374227c3844d40d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-26 15:05:06 +0530

    Challenge :- 3

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -606,7 +606,7 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'bharat');
-*/
+
 
 // Lecture :- Other Promise Combinators: race, allSettled and any
 
@@ -667,3 +667,28 @@ Promise.any([
   .catch((err) => console.error(err));
 
 // it takes an array of multiple promises and return first one fullfiled promise only
+
+// Challenge :- 3
+Your tasks:
+
+PART 1 
+1. Write an async function 'loadNPause' that recreates Challenge #2, this time 
+using async/await (only the part where the promise is consumed, reuse the 
+'createImage' function from before) 
+2. Compare the two versions, think about the big differences, and see which one 
+you like more 
+3. Don't forget to test the error handler, and to set the network speed to “Fast 3G” 
+in the dev tools Network tab 
+
+PART 2 
+1. Create an async function 'loadAll' that receives an array of image paths 
+'imgArr' 
+2. Use .map to loop over the array, to load all the images with the 
+'createImage' function (call the resulting array 'imgs') 
+3. Check out the 'imgs' array in the console! Is it like you expected? 
+4. Use a promise combinator function to actually get the images from the array 
+5. Add the 'parallel' class to all the images (it has some CSS styles) 
+Test data Part 2: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img
+3.jpg']. To test, turn off the 'loadNPause' function 
+
+*/
