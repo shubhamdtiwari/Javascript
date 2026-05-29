c399c1a032ac1563e0947b854bd56d833e1ce36d
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-29 14:38:17 +0530

    adding more code

--- 17-Modern-JS-Modules-Tooling/clean.js
+++ 17-Modern-JS-Modules-Tooling/clean.js
@@ -8,3 +8,57 @@ var budget = [
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
+
+var limits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+var add = function (value, description, user) {
+  if (!user) user = 'jonas';
+  user = user.toLowerCase();
+
+  var lim;
+  if (limits[user]) {
+    lim = limits[user];
+  } else {
+    lim = 0;
+  }
+
+  if (value <= lim) {
+    budget.push({ value: -value, description: description, user: user });
+  }
+};
+add(10, 'Pizza 🍕');
+add(100, 'Going to movies 🍿', 'Matilda');
+add(200, 'Stuff', 'Jay');
+console.log(budget);
+
+var check = function () {
+  for (var el of budget) {
+    var lim;
+    if (limits[el.user]) {
+      lim = limits[el.user];
+    } else {
+      lim = 0;
+    }
+
+    if (el.value < -lim) {
+      el.flag = 'limit';
+    }
+  }
+};
+check();
+
+console.log(budget);
+
+var bigExpenses = function (limit) {
+  var output = '';
+  for (var el of budget) {
+    if (el.value <= -limit) {
+      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+    }
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
