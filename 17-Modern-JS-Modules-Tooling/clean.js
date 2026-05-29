e7157c6bff928d038e10f284d63604b3222f0154
Shubham Tiwari <shubhamtpandit@gmail.com>
2026-05-29 14:37:32 +0530

    adding clean.js

---
+++ 17-Modern-JS-Modules-Tooling/clean.js
@@ -0,0 +1,10 @@
+var budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
